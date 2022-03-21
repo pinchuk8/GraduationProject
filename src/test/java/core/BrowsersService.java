0a7cb3800da01c0849e04b4260f5407e9da3dbc8
user <pinchuk759@gmail.com>
2022-03-21 14:21:01 +0300

    ГОТОВАЯ РАБОТА

--- src/test/java/core/BrowsersService.java
+++ src/test/java/core/BrowsersService.java
@@ -26,7 +26,6 @@ public class BrowsersService {
                 chromeOptions.addArguments("--disable-gpu");
                 chromeOptions.addArguments("--ignore-certificate-errors");
                 chromeOptions.addArguments("--silent");
-                // chromeOptions.addArguments("--start-maximized");
                 chromeOptions.addArguments("--kiosk");
 
                 driver = new ChromeDriver(chromeOptions);
